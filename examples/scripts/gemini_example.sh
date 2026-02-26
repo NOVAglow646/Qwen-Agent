776f0ef861e1cb43a39c29ac51d95dc74ccb5140
NOVAglow646 <1900012925@pku.edu.cn>
2026-02-26 18:53:23 +0800

    .

---
+++ examples/scripts/gemini_example.sh
@@ -0,0 +1,4 @@
+conda activate qwen_agent
+cd /ytech_m2v8_hdd/workspace/kling_mm/shiyang06/Project/Think_with_image/SAGE/Qwen-Agent
+export GOOGLE_APPLICATION_CREDENTIALS="/mmu_vcg_ssd/shiyang06/Tool/API/mmu-gemini-caption-1-5pro-86ec97219196.json"
+python -m examples.assistant_gemini2_5_pro
